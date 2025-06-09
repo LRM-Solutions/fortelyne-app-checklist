aa2a02c2b0662d9e4664da9ddb66e284a198846b
leonardovaloess <leonardoberlandadevaloes@gmail.com>
2025-06-09 14:46:49 -0300

    trocando rota da API

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,7 @@
 // Configurações da aplicação
 export const config = {
-  API_BASE_URL: "https://api-fortelyne.lrmsolutions.com.br/",
+  API_BASE_URL:
+    "https://ua37zhtbkigtbtukc7tn62jnn40ncoub.lambda-url.us-east-1.on.aws/",
   APP_NAME: "Fortelyne App",
   // Adicione outras configurações aqui conforme necessário
 };
