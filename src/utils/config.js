4dccc5d5fc6566e744fae0c84a58cbf506ef0c6d
leonardovaloess <leonardoberlandadevaloes@gmail.com>
2025-07-04 20:02:03 -0300

    fix route api

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,7 +1,6 @@
 // Configurações da aplicação
 export const config = {
-  API_BASE_URL:
-    "https://ua37zhtbkigtbtukc7tn62jnn40ncoub.lambda-url.us-east-1.on.aws/",
+  API_BASE_URL: "https://fortelyne-backend.vercel.app/",
   APP_NAME: "Fortelyne App",
   // Adicione outras configurações aqui conforme necessário
 };
